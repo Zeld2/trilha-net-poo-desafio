bd1973858c2fcf5779cad64fa02c778138e5c805
MatheusDelci <theudelci@gmail.com>
2023-01-17 21:12:14 -0300

    Desafio completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone iphoneMatheus = new Iphone("999999", "Xs", "000000-00-000000-0", 8);
+Smartphone nokiaGabi = new Nokia("88888888", "Aa", "000000-00-000000-1", 12);
+
+nokiaGabi.Ligar();
+iphoneMatheus.ReceberLigacao();
+
+nokiaGabi.InstalarAplicativo("Whatsapp");
+iphoneMatheus.InstalarAplicativo("Waze");
